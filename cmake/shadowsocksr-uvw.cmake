9121ad53366eedcf934319d80cbb94a338f70ad3
DuckVador <Lx3JQkmzRS@protonmail.com>
2020-10-11 00:09:52 +0800

    submodule: updated submodule

--- cmake/shadowsocksr-uvw.cmake
+++ cmake/shadowsocksr-uvw.cmake
@@ -1,2 +1,2 @@
-add_subdirectory(${CMAKE_SOURCE_DIR}/3rdparty/shadowsocksr-uvw)
+add_subdirectory(${CMAKE_SOURCE_DIR}/3rdparty/shadowsocksr-uvw EXCLUDE_FROM_ALL)
 SET(SHADOWSOCKSR_UVW_LIBRARY shadowsocksr::uvw::qt)
